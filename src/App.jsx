0ffdf424890a9ed5681f919da1f849574464bec3
jeff <jeffdelmotte@gmail.com>
2025-09-18 10:02:46 +0200

    creations des routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import Header from "./components/Header/Header";
+import DashboardPage from "./pages/DashboardPage";
+import NewArticle from "./pages/Dashboard/NewArticle";
+import EditArticle from "./pages/Dashboard/EditArticle";
+import BlogArticles from "./components/BlogArticles/BlogArticles";
+
 function App() {
   return (
-    <div>
-      <h1>Hello Vite + React</h1>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/dashboard/new" element={<NewArticle />} />
+        <Route path="/dashboard/edit/:id" element={<EditArticle />} />
+        <Route path="/blog/:blogName" element={<BlogArticles />} />
+      </Routes>
+    </Router>
   );
 }
 
