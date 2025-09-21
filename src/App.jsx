4f9175309eca99a67902f1bff9a60f107a030ef2
jeff <jeffdelmotte@gmail.com>
2025-09-21 11:32:25 +0200

    creation des routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import LoginPage from "./pages/LoginPage";
+
 import Header from "./components/Header/Header";
 import DashboardPage from "./pages/DashboardPage";
 import NewArticle from "./pages/Dashboard/NewArticle";
 import EditArticle from "./pages/Dashboard/EditArticle";
 import BlogArticles from "./components/BlogArticles/BlogArticles";
+import ArticleDetail from "./components/ArticleDetail/ArticleDetail";
+import DashboardComments from "./pages/DashboardComments"; // ✅ nouvelle page
+import AdminRoute from "./components/AdminRoute/AdminRoute";
 
 function App() {
   return (
@@ -13,11 +16,23 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
+
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/dashboard/new" element={<NewArticle />} />
         <Route path="/dashboard/edit/:id" element={<EditArticle />} />
+
+        {/* ✅ Gestion des commentaires avec AdminRoute */}
+        <Route
+          path="/admin/comments"
+          element={
+            <AdminRoute>
+              <DashboardComments />
+            </AdminRoute>
+          }
+        />
+
         <Route path="/blog/:blogName" element={<BlogArticles />} />
+        <Route path="/article/:slug" element={<ArticleDetail />} />
       </Routes>
     </Router>
   );
