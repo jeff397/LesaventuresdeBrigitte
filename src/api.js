d6e9896326feb780f636ecc5525c87cf7e6fc42b
jeff <jeffdelmotte@gmail.com>
2025-09-22 12:45:32 +0200

    preparation pour déploiement

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { API_URL } from "./config";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${API_URL}/api`,
 });
 
+// Intercepteur pour le token
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
