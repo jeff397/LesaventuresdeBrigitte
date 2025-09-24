15b1d3754054e1ad3d8eda7fdcd0c63dd7429980
jeff <jeffdelmotte@gmail.com>
2025-09-24 15:02:26 +0200

    correction de la requête api

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const getCategories = async () => {
   try {
-    const res = await axios.get("http://localhost:5000/api/categories");
+    const res = await axios.get(`${API_URL}/api/categories`);
     return res.data;
   } catch (err) {
     console.error(err);
